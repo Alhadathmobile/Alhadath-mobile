22ef12eda1b22733854b9254c091c2957b91869a
Alhadathmobile <Alhadathmobile739@gmail.com>
2025-09-28 23:41:48 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -75,7 +75,7 @@ const PRODUCTS = [
     colors: [
     { id:"black",  label:"أسود",  hex:"#111827", image:"assets/images/a26-black.jpeg"  },
     { id:"green",  label:"أخضر",  hex:"#86efac", image:"assets/images/a26-mint.jpg" },
-    { id:"silver", label:"أبيض",   hex:"#FFFFFF", image:"assets/images/a26-white.jpg"}
+    { id:"white", label:"أبيض",   hex:"#FFFFFF", image:"assets/images/a26-white.jpg"}
   ],
     specs: [
       "شاشة: 6.7 بوصة Super AMOLED FHD+، 120Hz",
@@ -113,8 +113,8 @@ const PRODUCTS = [
     ],
     colors: [
     { id:"black",  label:"أسود",  hex:"#111827", image:"assets/images/a36-black.jpg"  },
-    { id:"green",  label:"بنفسجي",  hex:"#EE82EE", image:"assets/images/a36-awesome-violet.jpg" },
-    { id:"silver", label:"أبيض",   hex:"#FFFFFF", image:"assets/images/a36-awesome-white.jpg"}
+    { id:"violet",  label:"بنفسجي",  hex:"#EE82EE", image:"assets/images/a36-awesome-violet.jpg" },
+    { id:"white", label:"أبيض",   hex:"#FFFFFF", image:"assets/images/a36-awesome-white.jpg"}
   ],
     specs: [
       "شاشة: 6.7 بوصة Super AMOLED FHD+، 120Hz",
@@ -138,8 +138,8 @@ const PRODUCTS = [
     ],
     colors: [
     { id:"black",  label:"أسود",  hex:"#111827", image:"assets/images/a56-black.jpg"  },
-    { id:"green",  label:".زهر",  hex:"#FFC0CB", image:"assets/images/a56-pink.jpg" },
-    { id:"silver", label:"رمادي",   hex:"#D3D3D3", image:"assets/images/a56-awesome-lightgray.jpg"}
+    { id:"pink",  label:".زهر",  hex:"#FFC0CB", image:"assets/images/a56-pink.jpg" },
+    { id:"gray", label:"رمادي",   hex:"#D3D3D3", image:"assets/images/a56-awesome-lightgray.jpg"}
   ],
     specs: [
       "شاشة: 6.7 بوصة Super AMOLED FHD+، 120Hz",
@@ -162,8 +162,8 @@ const PRODUCTS = [
     ],
     colors: [
     { id:"black",  label:"أسود",  hex:"#0F0F0F", image:"assets/images/s25-titanium-black.jpg"  },
-    { id:"green",  label:"رمادي",  hex:"#878681", image:"assets/images/s25-titanium-gray.jpg" },
-    { id:"silver", label:"أبيض",   hex:"#FFFFFF", image:"assets/images/s25-titanium-white.jpg"}
+    { id:"gray",  label:"رمادي",  hex:"#878681", image:"assets/images/s25-titanium-gray.jpg" },
+    { id:"white", label:"أبيض",   hex:"#FFFFFF", image:"assets/images/s25-titanium-white.jpg"}
   ],
     specs: [
       "شاشة: 6.9 بوصة QHD+ Dynamic AMOLED 2X، 120Hz",
@@ -180,7 +180,7 @@ const PRODUCTS = [
   { id:"ip-16pm",
    title:"iPhone 16 Pro Max",
    brand:"iPhone",
-   image:"assets/images/iphone-16-pro-max.jpeg",
+   image:"assets/images/iphone-16-pro-max.jpg",
    variants: [
       { id: "256", label: "256GB ", price: 910 },
       { id: "512",   label: "512GB ",   price: 990 },
