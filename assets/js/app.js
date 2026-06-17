a5005a5d1531a11b98c3072e6558968754998ecc
Alhadathmobile <Alhadathmobile739@gmail.com>
2026-06-17 15:13:57 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -145,9 +145,9 @@ const PRODUCTS = [
     ],
     colors: [
     { id:"awesome-charcoal",  label:"Awesome Charcoal (كحلي)",  hex:"#2B2B2D", image:"assets/images/a37-black.webp"  },
-    { id:"awesome-Graygreen",  label:"Awesome Graygreen (أخضر رمادي)",  hex:"#7E857B", image:"assets/images/a37-graygreen.jfif" },
+    { id:"awesome-Graygreen",  label:"Awesome Graygreen (أخضر رمادي)",  hex:"#7E857B", image:"assets/images/a37-graygreen.webp" },
     { id:"awesome-lavender",  label:"Awesome Lavender (بنفسجي فاتح)",  hex:"#C9B0D9", image:"assets/images/a37-lavender.webp" },  
-    { id:"awesome-white", label:"Awesome White (أبيض )",   hex:"#F5F5F2", image:"assets/images/a37-white.jfif"}
+    { id:"awesome-white", label:"Awesome White (أبيض )",   hex:"#F5F5F2", image:"assets/images/a37-white.webp"}
   ],
     specs: [
       "شاشة: 6.7 بوصة Super AMOLED FHD+، 120Hz",
